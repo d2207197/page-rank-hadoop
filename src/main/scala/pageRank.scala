aefed483b0781109268766a188b92f95514aefa3
顏孜羲 <joseph.yen@gmail.com>
2014-04-27 02:51:04 +0800

    1 step step 1 finished

--- src/main/scala/pageRank.scala
+++ src/main/scala/pageRank.scala
@@ -1,10 +1,11 @@
 package cc.nlplab
 
-// import scala.language.implicitConversions
+import scala.language.implicitConversions
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.io.{NullWritable, LongWritable, Text, Writable, IntWritable}
+import org.apache.hadoop.io.{NullWritable, LongWritable, Text, Writable, IntWritable, WritableComparator, WritableComparable}
 
-import org.apache.hadoop.mapreduce.{ReduceContext, MapContext}
+
+import org.apache.hadoop.mapreduce.{ReduceContext, MapContext, Partitioner}
 import scamr.MapReduceMain
 import scamr.conf.{LambdaJobModifier, ConfigureSpeculativeExecution}
 import scamr.io.{InputOutput, TextArrayWritable}
@@ -22,8 +23,6 @@ import grizzled.slf4j.Logging
 
 
 object WritableConversions2 {
-  // implicit def textToString(value: Text): String = value.toString
-  // implicit def stringToText(value: String): Text = new Text(value)
   implicit def textArrayToTextArrayWritable(value: Array[Text]): TextArrayWritable =
     new TextArrayWritable(value.map((x: Text) => x))
   implicit def textArrayWritableToTextArray(value: TextArrayWritable): Array[Text] =
@@ -53,72 +52,53 @@ class ExtractInfoMapper(context: MapContext[_, _, _, _])
   private val titlePattern = raw"""(?<=<title>)[^<]+(?=</title>)""".r
   private val linkPattern = raw"""(?<=\[\[)[^\]]+(?=\]\])""".r
   override def map(offset: LongWritable, line: Text): Unit = {
-    val lineString = line.toString
-    titlePattern.findFirstIn(lineString) match {
+    titlePattern.findFirstIn(line:String) match {
       case Some(title) =>
-        linkPattern.findAllIn(lineString) foreach (link => emit(new Text(link), new Text(title)))
-        emit(new Text(title), new Text())
+        linkPattern.findAllIn(line:String) foreach (link =>
+          {
+            val (linkTrimed, titleTrimed) = (link.trim, title.trim)
+            emit(titleTrimed, linkTrimed)
+            emit(" " + linkTrimed, "")
+          }
+        )
       case None => warn("No Title Found: $offset")
     }
   }
 }
 
 class FilterLinksReducer(context: ReduceContext[_,_,_,_])
-    extends SimpleReducer[Text, Text, Text, Text](context) with Logging {
-
-  override def reduce(link: Text, titles: Iterator[Text]): Unit = {
-    val titlesList = titles.toList
-    if (titlesList contains (new Text())) {
-      println(s"""accept $link ${titlesList mkString " | " }""")
-      titlesList foreach (title  => if (title != (new Text())) emit(title, link))
-    }
-    else println(s"""reject $link ${titlesList mkString " | " }""")
-  }
-}
-
-class TextTextIdentityMapper(context: MapContext[_,_,_,_]) 
-    extends IdentityMapper[Text, Text](context)
-
-class ConcatLinksReducer(context: ReduceContext[_,_,_,_])
     extends SimpleReducer[Text, Text, Text, PageRankLinks](context) with Logging {
+
   override def reduce(title: Text, links: Iterator[Text]): Unit = {
-    val linksArray = links.toArray
-    if (linksArray.length > 0)
-      emit(title, new PageRankLinks(1, linksArray))
-    else
-    {
-      println(s"NULL title: $title")
-      emit(title, new PageRankLinks(1, Array("NULL")))
+    if (title startsWith " ") {
+
+      val linksSet: Set[Text] = links.toSet - new Text()
+      println(s"""accept $title ${linksSet mkString " | " }""")
+      if (linksSet.size > 0)
+        emit(title.trim, new PageRankLinks(1, linksSet.toArray: Array[Text]))
+      else
+      {
+        println(s"NULL title: $title")
+        emit(title.trim, new PageRankLinks(1, Array("NULL")))
+      }
     }
+    else println(s"""reject $title """)
   }
 }
 
-// import scamr.mapreduce.lib.SumReducer
 
-// class WordCountReducer(context: ReduceContext[_,_,_,_]) extends SumReducer[Text](context)
+// import scala.collection.JavaConversions._
 
-// class LongAndTextWritableComparable(tuple: (LongWritable, Text))
-// extends Tuple2WritableComparable[LongWritable, Text](tuple) {
-//   def this(a1: LongWritable, a2: Text) = this((a1, a2))
-//   def this() = this((new LongWritable, new Text))
-// }
-
-// class CombineCountAndWordIntoTupleMapper(context: MapContext[_, _, _, _])
-// extends SimpleMapper[Text, LongWritable, LongAndTextWritableComparable, NullWritable](context) {
-//   override def map(word: Text, count: LongWritable): Unit =
-//     emit(new LongAndTextWritableComparable(count, word), NullWritable.get)
-// }
-
-// class OutputSortedCountsReducer(context: ReduceContext[_, _, _, _])
-// extends SimpleReducer[LongAndTextWritableComparable, NullWritable, Text, LongWritable](context) {
-//   override def reduce(key: LongAndTextWritableComparable, ignored: Iterator[NullWritable]): Unit = emit(key._2, key._1)
-// }
-
+class TitlePartitioner extends Partitioner[Text, Text] {
+  override def getPartition(title: Text, link: Text, numPartitions: Int): Int = {
+    (title.trim.hashCode & Int.MaxValue) % numPartitions
+  }
+}
 
 class GroupComparator protected ()
-    extends WritableComparator(classOf[TextPairWC], true) {
-  override def compare(w1: WritableComparable, w2: WritableComparable): Int = {
-    w1.asInstanceOf[Text].toString.trim compareTo w2.asInstanceOf[Text].toString.trim
+    extends WritableComparator(classOf[Text], true) {
+  override def compare(w1: WritableComparable[_], w2: WritableComparable[_]): Int = {
+    w1.asInstanceOf[Text].trim compareTo w2.asInstanceOf[Text].trim
   }
 }
 
@@ -129,8 +109,9 @@ object ExampleSortedWordCountMapReduce extends MapReduceMain {
     val pipeline = MapReducePipeline.init(conf) -->  // hint: start by adding a data source with -->
     new InputOutput.TextFileSource(inputDirs) --> // hint: use --> to direct data into or out of a stage
     new MapReduceJob(classOf[ExtractInfoMapper], classOf[FilterLinksReducer], "step 1: Build Graph") ++
-    LambdaJobModifier { job => job.setGroupingComparatorClass(classOf[GroupComparator]) } -->
-    new MapReduceJob(classOf[TextTextIdentityMapper], classOf[ConcatLinksReducer], "step 2 concatenate links") --> 
+    LambdaJobModifier { job => job.setGroupingComparatorClass(classOf[GroupComparator]) } ++
+    LambdaJobModifier { job => job.setPartitionerClass(classOf[TitlePartitioner]) } -->
+    // new MapReduceJob(classOf[TextTextIdentityMapper], classOf[ConcatLinksReducer], "step 2 concatenate links") --> 
     new InputOutput.TextFileSink[Text, PageRankLinks](outputDir)
       // hint: use ++ to add ConfModifiers or JobModifiers to a TaskStage or a StandAloneJob
       // ConfigureSpeculativeExecution(false, false) ++
